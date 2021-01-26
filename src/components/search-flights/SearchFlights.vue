aacac0e9a6c8a7d786214b5a6986c0fd0c7a5fdc
Engin Sasmaz <engins.93@gmail.com>
2021-01-26 13:46:43 +0100

    wip: seardh flights component

---
+++ src/components/search-flights/SearchFlights.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="search-flight-form">
+      <multiselect
+        v-model="selectedFrom"
+        :options="optionsFrom"
+        placeholder="From"
+        label="name">
+      </multiselect>
+      <multiselect
+        v-model="selectedTo"
+        :options="optionsTo"
+        placeholder="To"
+        label="name">
+      </multiselect>
+
+      <div class="btn" @click="getFlightInformation">Search</div>
+    </div>
+    <template class="flight-information" v-if="isFetchingFlightInformation">
+      Fetching data
+    </template>
+    <template class="flight-information" v-if="!isFetchingFlightInformation && flightInformation">
+      {{ this.flightInformation.price }}
+    </template>
+  </div>
+</template>
+
+<script lang="js" src="./SearchFlights.js"></script>
+<style lang="scss" src="./search-flights.scss" scoped></style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
