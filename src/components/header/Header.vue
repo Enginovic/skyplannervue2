bd11edf705c4d069dffb5f4cc658bd162ba7faab
Engin Sasmaz <engins.93@gmail.com>
2021-01-26 19:43:51 +0100

    edited header component structure

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="logo">Sky Planner</div>
+  <div class="header">
+    <div class="logo">Sky Planner</div>
+  </div>
 </template>
 
 <script lang="js" src="./Header.js"></script>
